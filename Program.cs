832c15e71d30c0709182c187d5b11196c4668a77
Andrew Gray <37289566+andrewjgray206@users.noreply.github.com>
2020-08-11 15:19:33 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VirtualTill
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
