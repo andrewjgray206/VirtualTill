b2163b26476fdef1c03c849727d6d56e2ac7af55
Andrew Gray <37289566+andrewjgray206@users.noreply.github.com>
2020-08-11 15:25:54 +1000

    + Added some basic classes.

---
+++ Till.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VirtualTill
+{
+    class Till
+    {
+        private CashDeck _cashDeck; //each till needs it's own cashdeck.
+
+
+    }
+
+    public Till()
+    {
+
+    }
+}
